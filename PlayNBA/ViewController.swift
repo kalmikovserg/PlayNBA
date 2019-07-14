50f2914f562e184e2e569a4e24cfc35e9fe42218
kalmykov1981 <kalmikovw13@gmail.com>
2019-07-14 18:09:53 -0500

    v1.1

--- PlayNBA/ViewController.swift
+++ PlayNBA/ViewController.swift
@@ -6,70 +6,71 @@
 //  Copyright © 2019 Сергей Калмыков. All rights reserved.
 //
 
-import UIKit
-import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
+
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        configuration.planeDetection = .vertical
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
+    
+    @IBAction func screenTaped(_ sender: UITapGestureRecognizer) {
+        let touchLacation = sender.location(in: sceneView)
+        let hitTestResult = sceneView.hitTest(touchLacation, types: .existingPlaneUsingExtent)
+        if let nearTestResult = hitTestResult.first {
+            addHoop(result: nearTestResult)
+        }
+    }
+    
+    //MARK: - Custom Methods
+    private func addHoop(result: ARHitTestResult) {
+        let hoop = SCNScene(named: "art.scnassets/hoop.scn")!.rootNode.clone()
+        hoop.simdTransform = result.worldTransform
+        hoop.eulerAngles.x -= .pi / 2
+        sceneView.scene.rootNode.addChildNode(hoop)
+        sceneView.scene.rootNode.enumerateChildNodes{ node, _ in
+            if node.name == "Wall" {
+                node.removeFromParentNode()
+            }
+        }
+    }
+    
+    private func creatWall(pleneAnchor: ARPlaneAnchor) -> SCNNode {
+        let extent = pleneAnchor.extent
+        let width = CGFloat(extent.x)
+        let hight = CGFloat(extent.z)
+        let plane = SCNPlane(width: width, height: hight)
+        let wall = SCNNode(geometry: plane)
+        plane.firstMaterial?.diffuse.contents = UIColor.red
+        wall.eulerAngles.x = -.pi / 2
+        wall.name = "Wall"
+        wall.opacity = 0.125
+       
+        return wall
+    }
+}
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        let wall = creatWall(pleneAnchor: planeAnchor)
+        node.addChildNode(wall)
     }
 }
