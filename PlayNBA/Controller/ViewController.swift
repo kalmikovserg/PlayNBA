728795c12b8b7987a549a1b64b647b85c3710e02
kalmykov1981 <kalmikovw13@gmail.com>
2019-07-17 22:00:18 -0500

    v1.2

---
+++ PlayNBA/Controller/ViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  PlayNBA
+//
+//  Created by Сергей Калмыков on 7/14/19.
+//  Copyright © 2019 Сергей Калмыков. All rights reserved.
+//
+
+import ARKit
+
+class ViewController: UIViewController {
+    
+    var scoreNBA = 0
+    
+    var isHoopPlaced = false {
+        didSet {
+            if isHoopPlaced {
+                guard let configuration = sceneView.session.configuration as? ARWorldTrackingConfiguration else { return }
+                configuration.planeDetection = []
+                sceneView.session.run(configuration)
+            }
+        }
+    }
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .vertical
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    @IBAction func screenTaped(_ sender: UITapGestureRecognizer) {
+        if isHoopPlaced {
+            addBall()
+        } else {
+            let touchLacation = sender.location(in: sceneView)
+            let hitTestResult = sceneView.hitTest(touchLacation, types: .existingPlaneUsingExtent)
+            if let nearTestResult = hitTestResult.first {
+                addHoop(result: nearTestResult)
+                isHoopPlaced = true
+            }
+        }
+    }
+    
+    private func addBall() {
+        guard let frame = sceneView.session.currentFrame else { return }
+        
+        let ball = SCNNode(geometry: SCNSphere(radius: 0.25))
+        ball.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ball, options: [SCNPhysicsShape.Option.collisionMargin: 0.01]))
+        ball.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "art.scnassets/basketballtexture.jpg")
+        let tranform = SCNMatrix4(frame.camera.transform)
+        let power = Float(10)
+        let force = SCNVector3(-tranform.m31 * power, -tranform.m32 * power, -tranform.m33 * power)
+        ball.physicsBody?.applyForce(force, asImpulse: true)
+        sceneView.scene.rootNode.addChildNode(ball)
+    }
+    
+    //MARK: - Custom Methods
+    private func addHoop(result: ARHitTestResult) {
+        let hoop = SCNScene(named: "art.scnassets/hoop.scn")!.rootNode.clone()
+        
+        hoop.simdTransform = result.worldTransform
+        hoop.eulerAngles.x -= .pi / 2
+        sceneView.scene.rootNode.addChildNode(hoop)
+        
+        hoop.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: hoop, options: [SCNPhysicsShape.Option.type: SCNPhysicsShape.ShapeType.concavePolyhedron]))
+        
+        let score = createScore()
+        
+        score.eulerAngles.y += 0.1
+        score.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(score)
+        
+        sceneView.scene.rootNode.enumerateChildNodes{ node, _ in
+            if node.name == "Wall" {
+                node.removeFromParentNode()
+            }
+            
+        }
+    }
+    
+    private func creatWall(pleneAnchor: ARPlaneAnchor) -> SCNNode {
+        let extent = pleneAnchor.extent
+        let width = CGFloat(extent.x)
+        let hight = CGFloat(extent.z)
+        let plane = SCNPlane(width: width, height: hight)
+        let wall = SCNNode(geometry: plane)
+        plane.firstMaterial?.diffuse.contents = UIColor.red
+        wall.eulerAngles.x = -.pi / 2
+        wall.name = "Wall"
+        wall.opacity = 0.125
+        
+        return wall
+    }
+    
+    private func createScore(nbaLabel: String = "Play BasketBall") -> SCNNode {
+        
+        let nbaGEO = SCNText(string: nbaLabel, extrusionDepth: 0.5)
+        nbaGEO.name = "nbaLabel"
+        nbaGEO.firstMaterial?.diffuse.contents = UIColor.yellow
+        let nbaNode = SCNNode(geometry: nbaGEO)
+        
+        return nbaNode
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        let wall = creatWall(pleneAnchor: planeAnchor)
+        node.addChildNode(wall)
+    }
+}
+
+
---
+++ PlayNBA/Model/Ball.swift
@@ -0,0 +1,14 @@
+//
+//  Ball.swift
+//  PlayNBA
+//
+//  Created by Сергей Калмыков on 7/17/19.
+//  Copyright © 2019 Сергей Калмыков. All rights reserved.
+//
+
+enum Point {
+    case threePoint
+    case twoPoint
+}
+
+
